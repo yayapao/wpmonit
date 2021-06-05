f2934969f6f70e611ae5738ce65a3948f70f9abd
Y-lonelY <luckyyoung666@gmail.com>
2021-06-05 19:03:57 +0800

    feat: add os handle

--- src/atom/os.ts
+++ src/atom/os.ts
@@ -1,8 +1,9 @@
 import { WN } from './constant'
+import parser from 'ua-parser-js'
+import { pick } from 'lodash'
 
 export function getOSInfo() {
-  return {
-    userAgent: WN.userAgent,
-    appVersion: WN.appVersion
-  }
+  const ua = parser(WN.userAgent)
+  const res = pick(ua, ['browser', 'engine', 'os'])
+  return res
 }
